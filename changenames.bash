760a11ab08ae2008f99e3a0299f6a2fa43da649f
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-04 16:42:20 +0800

    looks like is working

--- changenames.bash
+++ changenames.bash
@@ -1,12 +1,9 @@
 # simple bash utilty to change filenames in target dir from AA.BB.CC.abc to CC.BB.AA.abc
 target="target"
-for filepath in "$target/*"
+cd $target
+for filename in *
 do 
-	echo $filepath
-	filename=${filepath:${#target}+1} # this slice the string without foldername at the beginning
 	splittedNames=(${filename//./ }) # this split the string by '.'
 	newName="${splittedNames[2]}.${splittedNames[1]}.${splittedNames[0]}.${splittedNames[3]}" # this reconstruct the string in reversed order
-	newPath="$target/$newName"
-	echo $filepath $newPath
-	mv $filepath $newPath  # rename files
+	mv $filename $newName  # rename files
 done
